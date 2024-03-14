4722912acac232f65ffa610c7e2fa1cf67e21883
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2024-03-14 10:40:28 -0300

    Portfolio update icons

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './styleBody.css';
 import './medias.css'
 
+import github from '../images/LogosSocial/github-white.png'
+import linkedin from '../images/LogosSocial/linkedin-white.png'
+import gmail from '../images/LogosSocial/gmail-white.png'
+
 
 export default function Container() {
     return (
@@ -22,6 +26,11 @@ export default function Container() {
                 <div className='dvText'>
                     <p>Rodrigo Rimbau Desarrollador Web Full Stack</p>
                     <div className='sub'></div>
+                    <div className='network'>
+                        <a onClick={() => { window.open("https://github.com/rrodry") }}><img src={github} />  </a>
+                        <a onClick={() => { window.open("https://www.linkedin.com/in/rodrigo-rimbau/") }}><img src={linkedin} />  </a>
+                        <a href='mailto:rodrigorimbau94@gmail.com'><img src={gmail} /></a>
+                    </div>
                 </div>
             </div>
             <div className='moveArrow'>
